60308721419f9b5f4c12ae369c739400540b07e4
Michael Spieler <michael.spieler@epfl.ch>
2018-02-16 15:50:12 +0100

    Pixracer: change USB descriptor strings

--- target/pixracer/src/usbcfg.c
+++ target/pixracer/src/usbcfg.c
@@ -150,23 +150,19 @@ static const uint8_t vcom_string0[] = {
  * Vendor string.
  */
 static const uint8_t vcom_string1[] = {
-  USB_DESC_BYTE(38),                    /* bLength.                         */
+  USB_DESC_BYTE(34),                    /* bLength.                         */
   USB_DESC_BYTE(USB_DESCRIPTOR_STRING), /* bDescriptorType.                 */
-  'S', 0, 'T', 0, 'M', 0, 'i', 0, 'c', 0, 'r', 0, 'o', 0, 'e', 0,
-  'l', 0, 'e', 0, 'c', 0, 't', 0, 'r', 0, 'o', 0, 'n', 0, 'i', 0,
-  'c', 0, 's', 0
+  'S', 0, 'p', 0, 'i', 0, 'e', 0, 'l', 0, 'e', 0, 'r', 0, ' ', 0,
+  'B', 0, 'r', 0, 'o', 0, 't', 0, 'h', 0, 'e', 0, 'r', 0, 's', 0
 };
 
 /*
  * Device Description string.
  */
 static const uint8_t vcom_string2[] = {
-  USB_DESC_BYTE(56),                    /* bLength.                         */
+  USB_DESC_BYTE(18),                    /* bLength.                         */
   USB_DESC_BYTE(USB_DESCRIPTOR_STRING), /* bDescriptorType.                 */
-  'C', 0, 'h', 0, 'i', 0, 'b', 0, 'i', 0, 'O', 0, 'S', 0, '/', 0,
-  'R', 0, 'T', 0, ' ', 0, 'V', 0, 'i', 0, 'r', 0, 't', 0, 'u', 0,
-  'a', 0, 'l', 0, ' ', 0, 'C', 0, 'O', 0, 'M', 0, ' ', 0, 'P', 0,
-  'o', 0, 'r', 0, 't', 0
+  'P', 0, 'i', 0, 'x', 0, 'r', 0, 'a', 0, 'c', 0, 'e', 0, 'r', 0
 };
 
 /*
